c703e20457caaea2866e2f91bae72f1a340507dc
mahulkko <mahulkko@3DLAB3.in.fhkn.de>
2014-04-23 14:17:46 +0200

    Model: Working on the way algorithm.

--- TowerDefenceSS2014/src/de/htwg/towerdefence2014/model/way/IGraph.java
+++ TowerDefenceSS2014/src/de/htwg/towerdefence2014/model/way/IGraph.java
@@ -3,49 +3,111 @@ package de.htwg.towerdefence2014.model.way;
 import java.util.List;
 import de.htwg.towerdefence2014.util.Edge;
 
+/**
+* <b>Interface IGraph</b>
+* @author Christoph Knetschke and Martin Hulkkonen
+*/
 public interface IGraph<V> {
 	
- // Adds node to the graph
- boolean addVertex(V v);
+	/**
+	 * <b>Add a node to the graph</b>
+	 * @param v - Vertex to add
+	 * @return Return <b>true</b> when the vertex can be add, otherwise <b>false</b>
+	 */
+	boolean addVertex(V v);
  
- // Adds a new Edge to the graph with weight 1.0
- boolean addEdge(V v, V w);
- 
- // Adds a new Edge to the graph with weight
- boolean addEdge(V v, V w, double weight);
- 
- // delete a Edge between v and w
- boolean delteEdge(V v, V w); 
- 
- // delete all Edge to this Point
- boolean deleteAllEdgeOn(V v); 
- 
- // Checks if graph contains node
- boolean containsVertex(V v);  
- 
- // Checks if graph contains edge
- boolean containsEdge(V v, V w);
- 
- // Returns the weight of the edge
- double getWeight(V v, V w);  
- 
- // Returns the number of Nodes
- int getNumberOfVertexes(); 
- 
- // Returns the number of edge
- int getNumberOfEdges();  
- 
- // Returns a list of all Nodes
- List<V> getVertexList();  
- 
- // Returns a list of all edge
- List<Edge<V>> getEdgeList();  
- 
- // Get a list of all neighbor nodes
- List<V> getAdjacentVertexList(V v);  
- 
- // get a list of all neighbor edges
- List<Edge<V>> getIncidentEdgeList(V v);
+	/**
+	 * <b>Adds a new edge to the graph with the weight of 1.0</b>
+	 * @param v - Start vertex
+	 * @param w - End vertex
+	 * @return Returns <b>true</b> when the edge can be added, otherwise <b>false</b>
+	 */
+	boolean addEdge(V v, V w);
+	 
+	/**
+	 * <b>Adds a new edge to the graph with the given weight</b>
+	 * @param v - Start vertex
+	 * @param w - End vertex
+	 * @param weight - Weight of the edge
+	 * @return Returns <b>true</b> when the edge can be added, otherwise <b>false</b>
+	 */
+	boolean addEdge(V v, V w, double weight);
+	
+	/**
+	 * <b>Deletes a edge between two vertexes</b>
+	 * @param v - Start vertex
+	 * @param w - End vertex
+	 * @return Returns <b>true</b> when the edge can be deleted, otherwise <b>false</b>
+	 */
+	boolean delteEdge(V v, V w); 
+	 
+	/**
+	 * <b>Deletes all edges that references to this point</b>
+	 * @param v - Vertex
+	 * @return Returns <b>true</b> when the edges can be deleted, otherwise <b>false</b>
+	 */
+	boolean deleteAllEdgeOn(V v); 
+	 
+	/**
+	 * <b>Check if the graph contains this vertex</b>
+	 * @param v - Vertex
+	 * @return Returns <b>true</b> when the vertex is in the graph, otherwise <b>false</b>
+	 */
+	boolean containsVertex(V v);  
+	 
+	/**
+	 * <b>Check if the graph contains these edge between two vertexes</b>
+	 * @param v - Start vertex
+	 * @param w - End vertex
+	 * @return Returns <b>true</b> when the edge exists, otherwise <b>false</b>
+	 */
+	boolean containsEdge(V v, V w);
+	 
+	/**
+	 * <b>Get the weight of the edge between two vertexes</b>
+	 * @param v - Start vertex
+	 * @param w - End vertex
+	 * @return Returns the weight of the vertex
+	 */
+	double getWeight(V v, V w);  
+	 
+	/**
+	 * <b>Returns the number of vertexes in a graph</b>
+	 * @return Returns a number of vertexes in the graph
+	 */
+	int getNumberOfVertexes(); 
+	 
+	/**
+	 * <b>Get the number of edges in a graph</b>
+	 * @return Returns the number of edges in one graph
+	 */
+	int getNumberOfEdges();  
+	 
+	/**
+	 * <b>Get a list of all vertexes in the graph</b>
+	 * @return Returns a list of vertexes
+	 */
+	List<V> getVertexList();  
+	
+	/**
+	 * <b>Get a list of all edges in the graph</b>
+	 * @return Returns a list of edges
+	 */
+	List<Edge<V>> getEdgeList();  
+	 
+	/**
+	 * <b>Get all neighbors vertexes from one vertex</b>
+	 * @param v - Vertex
+	 * @return Returns a list of all neighbors vertexes
+	 */
+	List<V> getAdjacentVertexList(V v);  
+	 
+	/**
+	 * <b>Get all neighbors edges from one vertex</b>
+	 * @param v - Vertex
+	 * @return Returns a list of all neighbors edges
+	 */
+	List<Edge<V>> getIncidentEdgeList(V v);
  
 }
 
--- TowerDefenceSS2014/src/de/htwg/towerdefence2014/model/way/IUndirectedGraph.java
+++ TowerDefenceSS2014/src/de/htwg/towerdefence2014/model/way/IUndirectedGraph.java
@@ -1,8 +1,16 @@
 package de.htwg.towerdefence2014.model.way;
 
-
+/**
+* <b>IUndirectedGraph</b>
+* @author Christoph Knetschke and Martin Hulkkonen
+*/
 public interface IUndirectedGraph<V> extends IGraph<V> {
-	// Returns the value of node
+	
+	/**
+	 * <b>Get the value of the node</b>
+	 * @param v - Node
+	 * @return Returns the value of the node
+	 */
     int getDegree(V v); 
 }
 
