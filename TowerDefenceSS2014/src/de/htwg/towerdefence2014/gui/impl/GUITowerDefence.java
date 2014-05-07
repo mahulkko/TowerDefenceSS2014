d017fa3f6e9d92ad4cbb2e2183407806564f29dd
mahulkko <mahulkko@3DLAB2.in.fhkn.de>
2014-05-07 12:58:29 +0200

    GUI: Made some first test with swing gui.

--- TowerDefenceSS2014/src/de/htwg/towerdefence2014/gui/impl/GUITowerDefence.java
+++ TowerDefenceSS2014/src/de/htwg/towerdefence2014/gui/impl/GUITowerDefence.java
@@ -1,37 +1,99 @@
 package de.htwg.towerdefence2014.gui.impl;
 
-import java.awt.Image;
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.IOException;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
-import javax.imageio.ImageIO;
+import javax.swing.BorderFactory;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class GUITowerDefence extends JFrame {
+public class GUITowerDefence extends JFrame implements ActionListener, MouseListener{
+	
+	public class TransparentButton extends JButton {
+		  public TransparentButton () {
+		    super();
+		    setBackground (new Color (255, 255, 255, 0));
+		    setOpaque (false);
+		  }
+		}
+	
+	String resources = System.getProperty("user.dir") + "\\res\\";
+	TransparentButton button;
+	Icon icon1;
+	Icon icon2;
 	
 	public GUITowerDefence() {
 		setTitle("TowerDefence2014");
 	    setSize(800, 700);
 	    setLocationRelativeTo(null);
 	    setDefaultCloseOperation(EXIT_ON_CLOSE);
+	    //setUndecorated(true);						//Nice feature for startup
+	    //this.setOpacity(0.5f);						//Nice feature for startup
+	    setBackground(new Color(200,100,0));
 	    this.setVisible(true);
 	    
-	    Icon icon1 = new ImageIcon( "button.png" );
-
-	    JButton button = new JButton();
+	    icon1 = new ImageIcon( resources + "button.png" );
+	    icon2 = new ImageIcon( resources + "button2.png" );
+	    
+	    button = new TransparentButton();
+	    button.setBorder(BorderFactory.createEmptyBorder());
+	    button.setContentAreaFilled(false);
 	    button.setIcon( icon1 );
-	   
+	    button.addActionListener(this);
+	    button.addMouseListener(this);
+	    
 	    JPanel panel = new JPanel();
 	    getContentPane().add(panel);
 	    panel.setLayout(null);
 	    
-	    button.setSize(100, 100);
+	    button.setSize(304, 109);
 	    panel.add(button);
+	    validate();
+	    System.out.println(resources + "button.png");
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent event) {
+		if(event.getSource() == this.button){
+            System.out.println("Button gedrueckt");
+        }
+	}
 
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		button.setIcon( icon2 );
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		button.setIcon( icon1 );
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 }
+
+
+
+
